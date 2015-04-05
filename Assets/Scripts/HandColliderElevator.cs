7b3e3f339f0c459104e6551a4f49f6a02a701800
David Dolsen <dolsend@umich.edu>
2015-04-05 18:07:09 -0400

    Committing to add files that were missed last time

---
+++ Assets/Scripts/HandColliderElevator.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HandColliderElevator : MonoBehaviour {
+	
+	public GameObject carlMan;
+	public string rightLeft;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (rightLeft == "R") {
+			transform.position = carlMan.GetComponent<ZigSkeleton> ().LeftWrist.position;
+		} else if (rightLeft == "L") {
+			transform.position = carlMan.GetComponent<ZigSkeleton> ().RightWrist.position;
+		}
+	}
+
+//	void OnCollisionEnter (Collision collision) {
+//		if (collision.gameObject.tag == "Button") {
+//			string buttonColor = collision.gameObject.GetComponent<ElevatorButton> ().color;
+//			if (buttonColor == "red"){
+//
+//			}
+//		}
+//	}
+}
---
+++ Assets/Scripts/InstructionHandler.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class InstructionHandler : MonoBehaviour {
+
+	public RoundHandler roundHandler;
+	public GameObject[] instructionShapes = new GameObject[5];
+	public Material[] mats = new Material[5];
+	public ElevatorButton[] buttons = new ElevatorButton[5];
+
+	int currentRound;
+	int maxRound;
+	string[] correctButtonOrder;
+
+	// Use this for initialization
+	void Start () {
+		int pickRandomColorIndex = -1;
+		correctButtonOrder = new string[5];
+		
+		List<string> buttonChoices = new List<string>();
+		buttonChoices.Add ("red");
+		buttonChoices.Add ("green");
+		buttonChoices.Add ("blue");
+		buttonChoices.Add ("orange");
+		buttonChoices.Add ("purple");
+		List<Material> matList = new List<Material>();
+		matList.Add (mats[0]);
+		matList.Add (mats[1]);
+		matList.Add (mats[2]);
+		matList.Add (mats[3]);
+		matList.Add (mats[4]);
+
+		for (int i = 0; i < 5; i++) {
+
+			pickRandomColorIndex = Random.Range (0, 5 - i);
+			correctButtonOrder[i] = buttonChoices [pickRandomColorIndex];
+			buttonChoices.RemoveAt (pickRandomColorIndex);
+			instructionShapes[i].GetComponent<Renderer>().material = matList [pickRandomColorIndex];
+			matList.RemoveAt (pickRandomColorIndex);
+		}
+
+		instructionShapes[0].SetActive (true);
+		instructionShapes[1].SetActive (false);
+		instructionShapes[2].SetActive (false);
+		instructionShapes[3].SetActive (false);
+		instructionShapes[4].SetActive (false);
+
+		roundHandler.GetComponent<RoundHandler> ().correctButtonOrder = correctButtonOrder;
+		currentRound = 1;
+		maxRound = 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.A)) {
+			NextRound ();
+		}
+	}
+
+	void NextRound(){
+		currentRound++;
+		if (currentRound == 2) {
+			instructionShapes[1].SetActive (true);
+		}
+		else if (currentRound == 3) {
+			instructionShapes[2].SetActive (true);
+		}
+		else if (currentRound == 4) {
+			instructionShapes[3].SetActive (true);
+		}
+		else if (currentRound == 5) {
+			instructionShapes[4].SetActive (true);
+		}
+		else if (currentRound > maxRound) {
+			print ("END GAME");
+		}
+		buttons[0].GetComponent<ElevatorButton> ().NextRound ();
+		buttons[1].GetComponent<ElevatorButton> ().NextRound ();
+		buttons[2].GetComponent<ElevatorButton> ().NextRound ();
+		buttons[3].GetComponent<ElevatorButton> ().NextRound ();
+		buttons[4].GetComponent<ElevatorButton> ().NextRound ();
+		roundHandler.GetComponent<RoundHandler> ().NextRound ();
+	}
+}
